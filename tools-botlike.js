9406cc79838e71e173c9308416ba39dc4424755a
Jaenudin Maulana Marpaung <resultlegal69@gmail.com>
2018-12-03 16:26:17 +0700

    Update tools-botlike.js

--- tools-botlike.js
+++ tools-botlike.js
@@ -97,7 +97,8 @@ const Excute = async function(User, sleep){
 console.log(chalk`{bold.green
   Ξ TITLE  : BOTLIKE TL v1 *Set Sleep.
   Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ UPDATE : ADD ITTYW by @siidhamar
+  Ξ RECODE : S I R E U M ( @siidhamar )
   Ξ STATUS : ITTYW : {bold.red Not Supported!}}
   `);
 
